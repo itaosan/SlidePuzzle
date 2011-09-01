5c1715d534970bc78d44925b50ae6997e58bfc85
itaosan <syusaku@gmail.com>
2011-09-01 20:49:52 +0900

    test1

--- SlidePuzzle/src/SlidePuzzle/Answer.java
+++ SlidePuzzle/src/SlidePuzzle/Answer.java
@@ -7,8 +7,29 @@ public class Answer {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test!");
-
+		Question a = new Question(3, 3, "aiueo");
+		System.out.println(a.getData());
+		
 	}
 
+}
+
+class Question{
+	private int row;
+	private int col;
+	private String data;
+	
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public Question(int row,int col,String data){
+		this.row = row;
+		this.col = col;
+		this.data = data;
+	}
 }

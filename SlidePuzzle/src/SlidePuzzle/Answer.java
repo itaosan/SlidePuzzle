73d8ee9a2a27d14ccb2ea152b96fcc6138e70185
tomihisa <tomihisa@000_SYS_127>
2011-09-02 12:23:47 +0900

    ok

--- SlidePuzzle/src/SlidePuzzle/Answer.java
+++ SlidePuzzle/src/SlidePuzzle/Answer.java
@@ -1,25 +1,56 @@
 package SlidePuzzle;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Answer {
 
 	/**
 	 * hogeee
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Question a = new Question(3, 3, "aiueo");
 		System.out.println(a.getData());
-		
+		try {
+			/* ファイルをオープンします。 */
+			BufferedReader br = new BufferedReader(new FileReader(
+					"problems.txt"));
+
+			/* ファイルを読み込みます。 */
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			/* ファイルをクローズします。 */
+			br.close();
+		} catch (IOException e) {
+			System.out.println(e + "例外が発生しました");
+		}
+
+		try {
+			/* FileWriter クラスのインスタンスを作成します。 */
+			FileWriter fw = new FileWriter("./answer.txt");
+			/* ファイルに書き込みます。 */
+			fw.write("write text !");
+			/* ファイルをクローズします。 */
+			fw.close();
+		} catch (IOException e) {
+			System.out.println(e + "例外が発生しました");
+		}
 	}
 
 }
 
-class Question{
+class Question {
 	private int row;
 	private int col;
 	private String data;
-	
+
 	public String getData() {
 		return data;
 	}
@@ -28,7 +59,7 @@ class Question{
 		this.data = data;
 	}
 
-	public Question(int row,int col,String data){
+	public Question(int row, int col, String data) {
 		this.row = row;
 		this.col = col;
 		this.data = data;
